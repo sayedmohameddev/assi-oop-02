396211bec335eee8e10720af3b2cb67537bb4f1f
sayed123 <sayedmohameddev@gmai.com>
2025-01-05 18:33:24 +0200

    Add project files.

---
+++ assi-oop-02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assi_oop_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
